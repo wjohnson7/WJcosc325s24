ce8f8e977ead431783960fa9811a808f5067f942
Brian Toone <brtoone@samford.edu>
2024-01-12 10:28:05 -0600

    examples from jan 10 2024

---
+++ examplecode/racket/higherorderfunctions.rkt
@@ -0,0 +1,14 @@
+#lang racket
+; defines a simple list for the examples
+(define numbers '(1 2 3 4 5))
+
+; demonstrates "map" and "anonymous functions"
+; applies the anonymous function to each member of a list and returns new list with all the results
+(define squared (map (lambda (x) (* x x)) numbers))
+
+; demonstrates "reduce" ... condensing a list down to a single number
+(define sum (foldl + 0 squared))
+(define product (foldl * 1 squared))
+
+; demonstrates filter for trimming down a list to a smaller list
+(define evenNumbers (filter (lambda (number) (even? number)) numbers)) 
